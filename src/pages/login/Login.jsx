5548ff21a3739c724cd61f092262e6478e32de3d
hamzaig <hamzaig@yahoo.com>
2022-02-18 01:11:13 +0500

    Made ui of login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import classes from "./login.module.css";
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 const Login = () => {
   const [showPassword, setShowPassowrd] = useState(false);
@@ -13,24 +12,24 @@ const Login = () => {
     <main className={classes.loginMain}>
       <div className={classes.loginHeadingDiv}>
         <h1>Login to ESMSystem</h1>
-        <h5>Don't have an account? <span><a href="#">Contact Us</a></span></h5>
+        <p>Don't have an account? <span><a href="#">Contact Us</a></span></p>
       </div>
       <div>
         <div className={classes.formDiv}>
           <form action="" className={classes.form} autoComplete='off'>
-            <div>
+            <div className={classes.userDiv}>
               <div><span>User Id</span></div>
-              <input type="text" name="userId" id="userId" placeholder='Enter User Id' />
+              <input className={classes.userId} type="text" name="userId" id="userId" placeholder='Enter User Id' />
             </div>
-            <div>
+            <div className={classes.passwordDiv}>
               <div><span>Password</span></div>
-              <input type={showPassword ? "text" : "password"} name="password" id="password" placeholder='Enter Password' />
+              <input className={classes.password} type={showPassword ? "text" : "password"} name="password" id="password" placeholder='Enter Password' />
               {showPassword ? <VisibilityOffIcon className={classes.passwordIcon} onClick={toggleShowPassword} /> : <VisibilityIcon className={classes.passwordIcon} onClick={toggleShowPassword} />}
             </div>
             <div>
-              <button type="submit">Login <ArrowForwardIcon /></button>
+              <button type="submit">Login</button>
             </div>
-            <div>
+            <div className={classes.forgot}>
               <span><a href="#">Forgot your password/user id?</a></span>
             </div>
           </form>
